200c582be9f1f9ddc3e22baf2d8065ebdf7a9725
Guiomar Niso <guiomar.niso@ctb.upm.es>
2021-04-07 18:39:02 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,62 +1,61 @@
 % brainlife.io App for Brainstorm MEEG data analysis
 %
+% 1) Import MEG + Convert to continuous (CTF) + Refine registration
+% 2) PSD on sensosrs (pre)
+% 3) Notch filter (60Hz and harmonics) + High bandpass (0.3 Hz)
+%
 % Author: Guiomar Niso
 %
 % Copyright (c) 2020 brainlife.io 
 %
 % Indiana University
 
+clc; close all; clear;
+
 disp(['0) My script has started']);
 
 %% Load config.json
+
 % Load inputs from config.json
 config = jsondecode(fileread('config.json'));
 
 %% Some paths
 
+<<<<<<< Updated upstream
 % Directory with the segmented anatomy (e.g. freesufer output)
 %AnatDir1 = [fullfile(config.output)];
 %disp(AnatDir1)
 %AnatDir = [fullfile(config.freesurfer)];
 %disp(AnatDir)
 
+=======
+>>>>>>> Stashed changes
 % Directory to store results
 ReportsDir = 'out_dir/';
 DataDir    = 'out_data/';
 
 % Directory to store brainstorm database
-% BrainstormDbDir = '/media/data/guiomar/brainstorm_db';
-BrainstormDbDir = 'brainstorm_db/';
+BrainstormDbDir = [pwd, '/brainstorm_db/']; % Full path
 
 %% Parameters
-ProtocolName = 'Protocol03'; % The protocol name has to be a valid folder name (no spaces, no weird characters...)
+ProtocolName = 'Protocol01'; % The protocol name has to be a valid folder name (no spaces, no weird characters...)
 SubjectName = 'Subject01';
 
 %% START BRAINSTORM
 disp(['1) Brainstorm should be started on server mode']);
 
-% Start Brainstorm
-% if ~brainstorm('status')
-%       brainstorm server local
-% end
-
 % Set Brainstorm database directory
-bst_set('BrainstormDbDir',BrainstormDbDir) % FULL PATH
-disp(bst_get('BrainstormDbDir'));
+bst_set('BrainstormDbDir',BrainstormDbDir) 
 
-% Tutorial 1
-disp(bst_get('BrainstormUserDir')); % HOME/.brainstom (operating system)
-disp(getenv('HOME'));
-disp(char(java.lang.System.getProperty('user.home')));
+% See Tutorial 1
+disp(['BrainstormDbDir:', bst_get('BrainstormDbDir')]);
+disp(['BrainstormUserDir:', bst_get('BrainstormUserDir')]); % HOME/.brainstom (operating system)
+disp(['HOME env:', getenv('HOME')]);
+disp(['HOME java:', char(java.lang.System.getProperty('user.home'))]);
             
 
 %% CREATE PROTOCOL 
 disp(['2) Create protocol']);
-
-% sProtocol.Comment = ProtocolName;
-% sProtocol.SUBJECTS = [home 'anat'];
-% sProtocol.STUDIES = [home 'data'];
-% db_edit_protocol('load',sProtocol);
 
 % Find existing protocol
 iProtocol = bst_get('Protocol', ProtocolName);
@@ -64,38 +63,26 @@ disp(['iProtocol: ',num2str(iProtocol)]);
 
 if ~isempty(iProtocol)
     % Delete existing protocol
-    disp(['Delete protocol']);
+    disp(['- Delete protocol']);
     gui_brainstorm('DeleteProtocol', ProtocolName);
     % Select the current procotol
     % gui_brainstorm('SetCurrentProtocol', iProtocol);
 end
 
 % Create new protocol
-disp(['Create new protocol']);
+disp(['- Create new protocol']);
 UseDefaultAnat = 1; 
 UseDefaultChannel = 0;
 gui_brainstorm('CreateProtocol', ProtocolName, UseDefaultAnat, UseDefaultChannel);
-
-disp(['Protocol created!']);
-
-% Start a new report
-bst_report('Start');
 % Reset colormaps
 bst_colormaps('RestoreDefaults', 'meg');
 
-%%
-% 1) Import MEG + Convert to continuous (CTF) + Refine registration
-% 2) PSD on sensosrs (pre)
-% 3) Notch filter (60Hz and harmonics) + High bandpass (0.3 Hz)
-%
-% Guiomar Niso, 26 May 2016 (v1)
-% Guiomar Niso, 6 May 2015 (v0)
+disp(['Protocol created!']);
 
-% -------------------------------------------------------------------------
+%% Parameters
 
 % Frequencies to filter with the noth (power line 60Hz and harmonics)
-% freqs_notch = [60, 120, 180, 240, 300, 360, 420, 480, 540, 600];
-freqs_notch = [60];
+freqs_notch = [60:60:600];
 
 % Filters
 highpass = 0.3;
@@ -105,15 +92,25 @@ lowpass = 0; % 0: no filter
 win_length = 10; % sec 2
 win_overlap = 0; % percentage 50
 
+<<<<<<< Updated upstream
 sFilesMEG = fullfile(config.fif);
 % =========================================================================
+=======
+>>>>>>> Stashed changes
 
 %% ==== 1) Import MEG files =======================================
 disp(['1) Import MEG file']);
 
 sFiles0 = [];
+% Start a new report
+bst_report('Start');
+
 
 % ** CTF **
+%
+% Path to the data
+% sFilesMEG = fullfile(config.ctf);
+%
 % % % Process: Create link to raw file    
 % % sFiles = bst_process('CallProcess', 'process_import_data_raw', ...
 % %     sFiles0, [], ...
@@ -131,6 +128,9 @@ sFiles0 = [];
 
 % ** FIF **
 
+% Path to the data
+sFilesMEG = fullfile(config.fif);
+
 % Process: Create link to raw file    
 sFiles = bst_process('CallProcess', 'process_import_data_raw', ...
     sFiles0, [], ...
@@ -143,10 +143,11 @@ sFiles = bst_process('CallProcess', 'process_import_data_raw', ...
 % % % Process: Refine registration
 % % bst_process('CallProcess', 'process_headpoints_refine', ...
 % %     sFiles, []);
+
 disp(['1) Create snapshot MEG data']);
 
 % ** Process: Snapshot: Sensors/MRI registration
-hFig = bst_process('CallProcess', 'process_snapshot', ...
+bst_process('CallProcess', 'process_snapshot', ...
     sFiles, [], ...
     'target', 1, ...  % Sensors/MRI registration
     'modality', 1, ...% MEG (All)
@@ -156,7 +157,7 @@ hFig = bst_process('CallProcess', 'process_snapshot', ...
     'contact_nimage', 12, ...
     'threshold', 30, ...
     'comment', '');
-saveas(hFig, 'test.jpg')
+
 
 %% ==== 2) PSD on sensors (before filtering) ======================
 
